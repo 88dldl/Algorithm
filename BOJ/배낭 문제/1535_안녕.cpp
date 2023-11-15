b94c3b157257224b4a50e81a9ef395b2838109cb
88dldl <chltnrgml0999@gmail.com>
2023-11-16 04:52:02 +0900

    [1535] 안녕.cpp

---
+++ BOJ/배낭 문제/1535_안녕.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int n;
+    int loss[21],get[21],dp[21][101];
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        cin>>loss[i];
+    }
+    for(int i=1;i<=n;i++){
+        cin>>get[i];
+    }
+    
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<101;j++){
+            if(loss[i]>=j){
+                dp[i][j]=dp[i-1][j];
+            }
+            else{
+                dp[i][j]=max(dp[i-1][j],dp[i-1][j-loss[i]]+get[i]);
+            }
+        }
+    }
+    cout<<dp[n][100];
+    
+    return 0;
+}
