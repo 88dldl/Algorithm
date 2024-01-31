07d944fb456f1e6cc7db1b307baea8cb914811ba
88dldl <chltnrgml0999@gmail.com>
2024-01-31 19:45:05 +0900

    [11052] 카드 구매하기

---
+++ BOJ/DP/11052.카드 구매하기.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n;
+long long cardPay[1001];
+
+void solve(){
+    for(int i=2;i<=n;i++){ 
+        for(int j=1;j<i;j++){
+           cardPay[i]=max(cardPay[i-j]+cardPay[j],cardPay[i]);
+        }
+    }
+}
+
+int main()
+{
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        cin>>cardPay[i];
+    }
+    solve();
+    cout<<cardPay[n];
+    return 0;
+}
