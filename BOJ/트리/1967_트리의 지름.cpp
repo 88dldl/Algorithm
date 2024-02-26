6e8c4ee8ff4b4dfcd2998fce4ab2e977c8aa2ec3
88dldl <chltnrgml0999@gmail.com>
2024-02-27 03:58:57 +0900

    [1967] 트리의 지름

---
+++ BOJ/트리/1967_트리의 지름.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+using namespace std;
+
+
+int n,sumof=0,point=0; 
+vector<pair<int,int>> v[10001]; 
+int visit[10001]={false};
+
+void dfs(int x,int len){
+    if(visit[x]) return;
+    
+    visit[x]=true;
+    
+    if(sumof<len){//최대값 갱신
+        sumof=len;
+        point = x;
+    }
+    for(int i=0;i<v[x].size();i++){
+        dfs(v[x][i].first,len+v[x][i].second);
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    cin>>n;
+    for(int i=0;i<n-1;i++){
+        int x,y,z;
+        cin>>x>>y>>z;
+        v[x].push_back({y,z});
+        v[y].push_back({x,z});
+    }
+    dfs(1,0);
+    
+    sumof=0;
+    memset(visit,false,sizeof(visit));
+    
+    dfs(point,0);
+    cout<<sumof;
+    return 0;
+}
