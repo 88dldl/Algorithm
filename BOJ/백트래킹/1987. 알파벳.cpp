a73edc71487eed474072632848bf6aef6a815236
88dldl <chltnrgml0999@gmail.com>
2024-07-28 21:05:29 +0900

    [1987] 알파벳

--- BOJ/백트래킹/1987. 알파벳.cpp
+++ BOJ/백트래킹/1987. 알파벳.cpp
@@ -10,18 +10,18 @@ int dy[4] = {0,-1,0,1};
 
 void solve(int x,int y, int count){
     maxnum = max(maxnum,count);
-
+    
     for(int i=0;i<4;i++){
         int nx = x + dx[i];
         int ny = y + dy[i];
 
-        if(nx>=0 && nx<r && ny>=0 && ny<c){
-            if(!visit[word[nx][ny]-'A']){
-                visit[word[nx][ny]-'A']=true;
-                solve(nx,ny,count+1);
-                visit[word[nx][ny]-'A']=false;
-            }
-        }
+        if(nx<0 || nx>=r || ny<0 || ny>=c) continue;
+        if(visit[word[nx][ny]-'A']) continue;
+        
+        check = true;
+        visit[word[nx][ny]-'A']=true;
+        solve(nx,ny,count+1);
+        visit[word[nx][ny]-'A']=false;
     }
 } 
 
